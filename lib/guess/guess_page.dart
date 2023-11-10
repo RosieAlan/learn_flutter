7ce96211299f1159c90d1e5d2320020f55e70086
linjh <516308181@qq.com>
2023-11-10 09:35:09 +0800

    动画使用与状态周期

--- lib/guess/guess_page.dart
+++ lib/guess/guess_page.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'result_notice.dart';
 import 'package:flutter/services.dart';
 import 'guess_app_bar.dart';
+
 class GuessPage extends StatefulWidget {
   const GuessPage({super.key, required this.title});
   final String title;
@@ -10,23 +11,38 @@ class GuessPage extends StatefulWidget {
   @override
   State<GuessPage> createState() => _GuessPageState();
 }
-class _GuessPageState extends State<GuessPage> {
+
+class _GuessPageState extends State<GuessPage> with TickerProviderStateMixin {
+  late AnimationController _controller;
   bool? _isBig;
   int _value = 0;
   bool _guessing = false;
   Random _random = Random();
-  TextEditingController _guessCtrl =TextEditingController();
+  TextEditingController _guessCtrl = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = AnimationController(
+      vsync: this, //vsync 用于指定 AnimationController 的 ticker，一般指定当前的 State
+      duration: const Duration(milliseconds: 200), //duration 用于指定动画的时长
+    );
+  }
+
   void _generateRandomValue() {
     setState(() {
       _guessing = true; // 点击按钮时，表示游戏开始
-      _value=_random.nextInt(100);
+      _value = _random.nextInt(100);
     });
   }
-  void _onCheck(){
+
+  void _onCheck() {
     print("---check $_value,猜的数字是${_guessCtrl.text}");
     int guessValue = int.parse(_guessCtrl.text);
     if (!_guessing || guessValue == null) return;
-    if (guessValue == _value){
+    _controller.forward(from: 0.0); //动画从头开始播放
+    if (guessValue == _value) {
       showDialog(
           context: context,
           builder: (context) {
@@ -49,8 +65,9 @@ class _GuessPageState extends State<GuessPage> {
       });
       return;
     } else {
-      setState(() {//猜错了，根据猜的数字和随机数值的大小，设置 _isBig 的值
-        _isBig = guessValue > _value;//猜的数字大了
+      setState(() {
+        //猜错了，根据猜的数字和随机数值的大小，设置 _isBig 的值
+        _isBig = guessValue > _value; //猜的数字大了
       });
     }
   }
@@ -58,38 +75,50 @@ class _GuessPageState extends State<GuessPage> {
   @override
   void dispose() {
     // TODO: implement dispose
-    _guessCtrl.dispose();//输入控制器有销毁的方法，需要覆写状态类的 dispose 方法，调用一下：
+    _guessCtrl.dispose(); //输入控制器有销毁的方法，需要覆写状态类的 dispose 方法，调用一下：
     // 也不知道为啥复写一下
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:GuessAppBar(
+      appBar: GuessAppBar(
         onCheck: _onCheck,
         controller: _guessCtrl,
       ),
-      body:Stack(
+      body: Stack(
         children: [
-          if(_isBig!=null)
-          Column(
-            children: [
-              if(_isBig!)//如果猜的数字大了，显示大了的提示
-              ResultNotice(color:Colors.greenAccent,info:'大了'),
-              Spacer(),
-              if(!_isBig!)//如果猜的数字小了，显示小了的提示
-              ResultNotice(color:Colors.blueAccent,info:'小了'),
-            ],
-          ),
+          if (_isBig != null)
+            Column(
+              children: [
+                if (_isBig!) //如果猜的数字大了，显示大了的提示
+                  ResultNotice(
+                    color: Colors.greenAccent,
+                    info: '大了',
+                    controller: _controller,
+                  ),
+                Spacer(),
+                if (!_isBig!) //如果猜的数字小了，显示小了的提示
+                  ResultNotice(
+                    color: Colors.blueAccent,
+                    info: '小了',
+                    controller: _controller,
+                  ),
+              ],
+            ),
           Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                if(!_guessing)
-                  const Text('点击生成随机数值',),
+                if (!_guessing)
+                  const Text(
+                    '点击生成随机数值',
+                  ),
                 Text(
                   _guessing ? '**' : '$_value',
-                  style: const TextStyle(fontSize: 68, fontWeight: FontWeight.bold),
+                  style: const TextStyle(
+                      fontSize: 68, fontWeight: FontWeight.bold),
                 ),
               ],
             ),
--- lib/guess/result_notice.dart
+++ lib/guess/result_notice.dart
@@ -1,28 +1,59 @@
 import 'package:flutter/material.dart';
 
-class ResultNotice extends StatelessWidget{
+class ResultNotice extends StatefulWidget {
   final Color color;
   final String info;
 
-    const ResultNotice({
-      Key? key,
-      required this.color,
-      required this.info,
-     }):super(key:key);
+  const ResultNotice({
+    Key? key,
+    required this.color,
+    required this.info, required AnimationController controller,
+  }) : super(key: key);
 
+  @override
+  State<ResultNotice> createState() => _ResultNoticeState();
+}
 
-    @override
+class _ResultNoticeState extends State<ResultNotice>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    );
+    controller.forward();
+  }
+
+  @override
   Widget build(BuildContext context) {
     // TODO: implement build
-     return Expanded(
+    return Expanded(
         child: Container(
-          alignment: Alignment.center,
-          color: color,
-          child: Text(
-            info,
-            style: TextStyle(
-                fontSize: 54, color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-        ));
+            alignment: Alignment.center,
+            color: Colors.indigoAccent,
+            child: AnimatedBuilder(
+                animation: controller,
+                builder: (_, child) => Text(widget.info,
+                    style: TextStyle(
+                      fontSize: 48 * controller.value,
+                      color: Colors.white,
+                    )))));
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant ResultNotice oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
   }
 }
